649de12d2cba7035768b9c8ecd6dfc63a727578e
Jose Carlos Gutierrez <joseco48@gmail.com>
2025-05-02 23:13:03 -0400

    Base objects for DDD implementation and Results Pattern

---
+++ src/Joseco.DDD.Core/Results/DomainException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joseco.DDD.Core.Results;
+
+public class DomainException(Error Error) : Exception
+{
+    public Error Error { get; } = Error;
+}
